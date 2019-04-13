566cedf2327523e20e83fef2dc865c548cc8f43b
Kousuke Kawahira <kawahira@binzume.net>
2019-04-13 16:06:39 +0900

    use req.WithContext()

--- hls_downloader.go
+++ hls_downloader.go
@@ -7,10 +7,10 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
 	"regexp"
 	"strings"
 	"time"
@@ -29,18 +29,18 @@ func NewHLSDownloader(client *http.Client) *HLSDownloader {
 	return &HLSDownloader{HttpClient: client}
 }
 
-func (c *HLSDownloader) getBytes(url string) ([]byte, error) {
-	return GetContent(c.HttpClient, url)
-}
-
-func (c *HLSDownloader) SaveKey(keyURL string) error {
-	key, err := c.getBytes(keyURL)
+func (c *HLSDownloader) getBytes(ctx context.Context, url string) ([]byte, error) {
+	req, err := NewGetReq(url, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	w, _ := os.Create("./hls.key")
-	_, err = w.Write(key)
-	return nil
+	req = req.WithContext(ctx)
+	res, err := c.HttpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	return ioutil.ReadAll(res.Body)
 }
 
 // Simple hls client...
@@ -57,11 +57,12 @@ func relative(base, relative string) string {
 	return u1.ResolveReference(u2).String()
 }
 
-func (c *HLSDownloader) GetSegment(url string, w io.Writer) error {
+func (c *HLSDownloader) GetSegment(ctx context.Context, url string, w io.Writer) error {
 	req, err := NewGetReq(url, nil)
 	if err != nil {
 		return err
 	}
+	req = req.WithContext(ctx)
 	res, err := c.HttpClient.Do(req)
 	if err != nil {
 		return err
@@ -71,9 +72,9 @@ func (c *HLSDownloader) GetSegment(url string, w io.Writer) error {
 	return err
 }
 
-func (c *HLSDownloader) GetSegment2(url string, w io.Writer, key, iv []byte) error {
+func (c *HLSDownloader) GetSegment2(ctx context.Context, url string, w io.Writer, key, iv []byte) error {
 	// TODO io.Reader/Writer
-	data, err := c.getBytes(url)
+	data, err := c.getBytes(ctx, url)
 	if err != nil {
 		return err
 	}
@@ -95,23 +96,23 @@ func (c *HLSDownloader) GetSegment2(url string, w io.Writer, key, iv []byte) err
 }
 
 func (c *HLSDownloader) Downaload(ctx context.Context, url string, w io.Writer) error {
-	res, err := c.getBytes(url)
+	res, err := c.getBytes(ctx, url)
 	if err != nil {
 		return err
 	}
 	playlist := string(res)
 
+	reqInterval := 100 * time.Millisecond
 	re := regexp.MustCompile(`^#EXT-X-KEY:METHOD=AES-128,URI="([^"]+)",IV=0x([0-9a-fA-F]+)`)
 	lines := strings.Split(playlist, "\n")
 	for i, line := range lines {
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
-		default:
+		case <-time.After(reqInterval):
 		}
 		if strings.HasPrefix(line, "#EXT-X-STREAM-INF:") {
 			// TODO: select better stream.
-			time.Sleep(1 * time.Second)
 			return c.Downaload(ctx, relative(url, lines[i+1]), w)
 		}
 		match := re.FindStringSubmatch(line)
@@ -121,7 +122,7 @@ func (c *HLSDownloader) Downaload(ctx context.Context, url string, w io.Writer) 
 			log.Println(keyURL)
 			log.Println(ivHex)
 			c.iv, _ = hex.DecodeString(ivHex)
-			c.key, err = c.getBytes(keyURL)
+			c.key, err = c.getBytes(ctx, keyURL)
 			if len(c.key) != 16 {
 				return fmt.Errorf("invalid key length : %v", len(c.key))
 			}
@@ -131,9 +132,9 @@ func (c *HLSDownloader) Downaload(ctx context.Context, url string, w io.Writer) 
 		}
 		if len(line) > 0 && !strings.HasPrefix(line, "#") {
 			if c.key != nil {
-				err = c.GetSegment2(relative(url, line), w, c.key, c.iv)
+				err = c.GetSegment2(ctx, relative(url, line), w, c.key, c.iv)
 			} else {
-				err = c.GetSegment(relative(url, line), w)
+				err = c.GetSegment(ctx, relative(url, line), w)
 			}
 			if err != nil {
 				return err
--- http_downloader.go
+++ http_downloader.go
@@ -2,7 +2,6 @@ package nigonigo
 
 import (
 	"context"
-	"errors"
 	"io"
 	"net/http"
 )
@@ -18,33 +17,18 @@ func NewHTTPDownloader(client *http.Client) *HTTPDownloader {
 	return &HTTPDownloader{HttpClient: client}
 }
 
-type wrappedReader struct {
-	reader io.Reader
-	done   <-chan struct{}
-}
-
-func (r *wrappedReader) Read(p []byte) (n int, err error) {
-	select {
-	case <-r.done:
-		return 0, errors.New("cancelled")
-	default:
-		return r.reader.Read(p)
-	}
-}
-
 func (c *HTTPDownloader) Downaload(ctx context.Context, url string, w io.Writer) error {
 	req, err := NewGetReq(url, nil)
 	if err != nil {
 		return err
 	}
 
-	res, err := c.HttpClient.Do(req)
+	res, err := c.HttpClient.Do(req.WithContext(ctx))
 	if err != nil {
 		return err
 	}
 
 	defer res.Body.Close()
-
-	_, err = io.Copy(w, &wrappedReader{res.Body, ctx.Done()})
+	_, err = io.Copy(w, res.Body)
 	return err
 }
