8fe8464da5d7e5a33911490127486fca8b8a347b
Kousuke Kawahira <kawahira@binzume.net>
2019-04-14 00:56:16 +0900

    add load/save session function.

---
+++ account.go
@@ -0,0 +1,126 @@
+package nigonigo
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type AccountConfig struct {
+	ID       string `json:"id"`
+	Password string `json:"password"`
+}
+
+type NicoSession struct {
+	NiconicoID    string `json:"niconicoId"`
+	IsPremium     bool   `json:"premium"`
+	SessionString string `json:"user_session"`
+}
+
+func (n *Client) LoginWithJsonFile(path string) error {
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	var c AccountConfig
+	err = json.Unmarshal(buf, &c)
+	if err != nil {
+		return err
+	}
+	return n.Login(&c)
+}
+
+func (c *Client) Login(ac *AccountConfig) error {
+	return c.LoginWithPassword(ac.ID, ac.Password)
+}
+
+func (c *Client) LoginWithPassword(id, password string) error {
+	params := map[string]string{
+		"mail_tel": id,
+		"password": password,
+	}
+	req, err := NewPostReq(loginApiUrl, params)
+	if err != nil {
+		return err
+	}
+
+	res, err := c.HttpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if strings.Contains(res.Request.URL.String(), "/account.nicovideo.jp/") {
+		return errors.New("login error")
+	}
+
+	cookie, err := res.Request.Cookie("user_session")
+	if err != nil {
+		return err
+	}
+
+	c.Session = &NicoSession{
+		NiconicoID:    res.Header.Get("X-Niconico-Id"),
+		IsPremium:     res.Header.Get("X-Niconico-Authflag") == "3",
+		SessionString: cookie.Value,
+	}
+	return err
+}
+
+func (c *Client) SaveLoginSession(path string) error {
+	json, err := json.Marshal(c.Session)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(path, json, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) LoadLoginSession(path string) error {
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	var s NicoSession
+	err = json.Unmarshal(buf, &s)
+	if err != nil {
+		return err
+	}
+	return c.SetSessionString(s.SessionString)
+}
+
+func (c *Client) SetSessionString(sessionStr string) error {
+	cookie := &http.Cookie{Name: "user_session", Value: sessionStr}
+	parsed, _ := url.Parse(topUrl)
+	c.HttpClient.Jar.SetCookies(parsed, []*http.Cookie{cookie})
+
+	req, err := NewPostReq(topUrl, nil)
+	if err != nil {
+		return err
+	}
+	res, err := c.HttpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.Header.Get("X-Niconico-Id") == "" {
+		return errors.New("expired")
+	}
+
+	c.Session = &NicoSession{
+		NiconicoID:    res.Header.Get("X-Niconico-Id"),
+		IsPremium:     res.Header.Get("X-Niconico-Authflag") == "3",
+		SessionString: sessionStr,
+	}
+	return nil
+}
--- niconico.go
+++ niconico.go
@@ -1,7 +1,6 @@
 package nigonigo
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"net/http"
 )
@@ -20,50 +19,12 @@ type Client struct {
 	Session    *NicoSession
 }
 
-type AccountConfig struct {
-	Id       string `json:"id"`
-	Password string `json:"password"`
-}
-
-type NicoSession struct {
-	Id            int64
-	SessionString string
-	IsPremium     bool
-}
-
 func NewClient() *Client {
 	client, err := NewHttpClient()
 	if err != nil {
 		panic(err)
 	}
 	return &Client{client, nil}
-}
-
-func (n *Client) LoginWithJsonFile(path string) error {
-	buf, err := ioutil.ReadFile(path)
-	if err != nil {
-		return err
-	}
-
-	var c AccountConfig
-	err = json.Unmarshal(buf, &c)
-	if err != nil {
-		return err
-	}
-	return n.Login(&c)
-}
-
-func (c *Client) Login(ac *AccountConfig) error {
-	params := map[string]string{
-		"mail_tel": ac.Id,
-		"password": ac.Password,
-	}
-	_, err := c.post(loginApiUrl, params)
-	if err != nil {
-		return err
-	}
-	// TODO
-	return nil
 }
 
 func (c *Client) GetContent(url string) ([]byte, error) {
